d65014b8bbbacae4a4468c179e9a8023509a111c
ggghosthat <ildarildar990@gmail.com>
2022-09-10 17:32:50 +0300

    handler can has its own id and active state; Procesor find out handlers by Guid struct not string hid; implement hashtable to communicate with handlers;

---
+++ Estr/Processors/Processor.cs
@@ -0,0 +1,53 @@
+using System.Net;
+using System.Net.Sockets;
+using Estr.Handlers;
+using System.Collections;
+using System;
+
+namespace Estr.Processor
+{
+    internal struct Processor : IProcessor
+    {
+        private Hashtable _handlers = new();
+
+
+        public Processor(){}
+
+        public void Process(TcpClient client)              
+        {
+        }
+
+        public void RegistHandler(IHandler handler)
+        {
+            if (!_handlers.Contains(handler.HandlerId))
+                _handlers.Add(handler.HandlerId, handler);
+
+        }
+
+        public void RemoveHandler(Guid hid)
+        {
+            if (_handlers.Contains(hid))
+                _handlers.Remove(hid);
+        }
+
+        public void RemoveAllHandlers()
+        {
+        }
+
+        public void ActivateAllHandlers()
+        {
+        }
+
+        public void DeactivateAllHandlers()
+        {
+        }
+
+        public void ActivateHandler(Guid hid)
+        {
+        }
+
+        public void DeactivateHandler(Guid hid)
+        {
+        }
+    }
+}
