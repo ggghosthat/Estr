8b06f9748b422a60c69beeabb20d5b922670fdac
ggghosthat <ildarildar990@gmail.com>
2022-10-06 08:39:53 +0300

    revance

--- Estr/Processors/Processor.cs
+++ Estr/Processors/Processor.cs
@@ -32,23 +32,29 @@ namespace Estr.Processor
 
         public void RemoveAllHandlers()
         {
-
+	    _handlers.Clear();
         }
 
         public void ActivateAllHandlers()
         {
+	    foreach (IHandler handler in _handlers.Values)
+		handler.Activate();
         }
 
         public void DeactivateAllHandlers()
         {
+	    foreach (IHandler handler in _handlers.Values)
+		handler.Deactivate();
         }
 
         public void ActivateHandler(Guid hid)
         {
+	    ((IHandler)_handlers[hid]).Activate();
         }
 
         public void DeactivateHandler(Guid hid)
         {
+	    ((IHandler)_handlers[hid]).Deactivate();
         }
     }
 }
---
+++ TestServer/Program.cs
@@ -0,0 +1,15 @@
+﻿using Estr.Host;
+using System;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            EstrHost host = new(null);
+            await host.StartAsync();
+        }
+    }
+}
